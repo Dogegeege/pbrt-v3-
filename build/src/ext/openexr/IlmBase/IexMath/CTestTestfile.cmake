8ac9d141a74b4581da41500a2e308ff54acafb83
Dogegeege <149315509+Dogegeege@users.noreply.github.com>
2025-03-18 22:56:27 +0800

    Delete build directory

--- build/src/ext/openexr/IlmBase/IexMath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Project/pbrt-v3/src/ext/openexr/IlmBase/IexMath
-# Build directory: E:/Project/pbrt-v3/build/src/ext/openexr/IlmBase/IexMath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/ext/ptex/src/ptex/CMakeFiles/Ptex_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "Ptex.lib"
-)
